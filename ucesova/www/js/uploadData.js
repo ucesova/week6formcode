938d80c9f0900eec25108c1375bd41365e778666
ucesova <ucesova@ucl.ac.uk>
2018-04-11 16:03:58 -0400

    uploading data from the form

---
+++ ucesova/www/js/uploadData.js
@@ -0,0 +1,59 @@
+function startDataUpload() {
+	alert ("start data upload");
+
+	var name = document.getElementById("name").value;
+	var surname = document.getElementById("surname").value;
+	var module = document.getElementById("module").value;
+
+	alert(name + " "+ surname + " "+module);
+	
+	//create a name/value pair string as parameters for the URL to send values to the server
+	var postString = "name="+name +"&surname="+surname+"&module="+module;
+	
+	// now get the checkbox values - separate them with a | so that they can be // split later on if necessary
+	var checkString = "";
+	for (var i = 1;i< 5;i++){
+		if (document.getElementById("check"+i).checked === true) {
+			checkString = checkString + document.getElementById("check"+i).value + "||"
+		}
+	}
+	
+	// now get the radio button values
+	if (document.getElementById("morning").checked) {
+ 		 postString=postString+"&lecturetime=morning";
+	}
+	if (document.getElementById("afternoon").checked) {
+ 		 postString=postString+"&lecturetime=afternoon";
+	}
+	
+	// now get the select box values
+	var language = document.getElementById("languageselectbox").value;
+	postString = postString + "&language="+language;
+
+	// now get the geometry values
+	var latitude = document.getElementById("latitude").value;
+	var longitude = document.getElementById("longitude").value;
+	postString = postString + "&latitude=" + latitude + "&longitude=" + longitude;
+
+	postString = postString + "&modulelist="+checkString;
+
+	processData(postString);
+}
+
+var client;
+
+function processData(postString) {
+   client = new XMLHttpRequest();
+   client.open('POST','http://developer.cege.ucl.ac.uk:30293/uploadData',true);
+   client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+   client.onreadystatechange = dataUploaded;  
+   client.send(postString);
+}
+// create the code to wait for the response from the data server, and process the response once it is received
+function dataUploaded() {
+  // this function listens out for the server to say that the data is ready - i.e. has state 4
+  if (client.readyState == 4) {
+    // change the DIV to show the response
+    document.getElementById("dataUploadResult").innerHTML = client.responseText;
+    }
+}
